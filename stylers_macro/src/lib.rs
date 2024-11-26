49a0b010ad6a3219cb95bfeb77380843bae53737
Moritz <moritz@voulter.com>
2024-11-26 14:42:59 +0100

    Fix path display

--- stylers_macro/src/lib.rs
+++ stylers_macro/src/lib.rs
@@ -34,7 +34,7 @@ pub fn style_sheet(ts: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut current = env::current_dir().expect("No current dir found");
     current.push(file_path);
     let css_content = std::fs::read_to_string(current)
-        .expect(format!("Expected to read file: {}", current).as_str());
+        .expect(format!("Expected to read file: {}", current.display()).as_str());
     let class = Class::rand_class_from_seed(css_content.to_string());
     let class = class.as_name();
     let expanded = quote! {
